7d9dfe0660a0875b7a266a4c0d8d992bcfede58f
sefa <erzan@code.id>
2025-07-02 00:14:59 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080", // container port mapped to host
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
       },
     },
